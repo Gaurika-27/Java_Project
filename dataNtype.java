9e903aaf07a507968650978ba0a9e56482983e75
Gaurika-27 <gaurika.gla_cs24@gla.ac.in>
2025-08-12 09:42:05 +0530

    Create dataNtype.java

---
+++ dataNtype.java
@@ -0,0 +1,10 @@
+public class dataNtype {
+    public static void main(String[] args) {
+        String a="gaurika";
+        int [] arr={1,2,3,4,5,6,7};
+    System.out.println(a);
+    System.out.println(arr[4]);
+  
+    }
+    
+}
