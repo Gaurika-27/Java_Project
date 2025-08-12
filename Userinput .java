9648e0864010f26c6693a9a26226f53e1bd95803
Gaurika-27 <gaurika.gla_cs24@gla.ac.in>
2025-08-12 09:42:59 +0530

    Create Userinput .java

---
+++ Userinput .java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Userinput{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a no.");
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        System.out.println(a+b);
+    }
+}
